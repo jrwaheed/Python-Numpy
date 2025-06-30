35974e6376a51cc6263a3f7377dbe09f7921a021
jrwaheed <j.r.waheed@gmail.com>
2025-06-30 21:00:46 +0200

    update

--- ArrayOrientedProgramming.py
+++ ArrayOrientedProgramming.py
@@ -16,3 +16,9 @@ plt.imshow(z, cmap='gray', extent=[-5, 5, -5, 5])
 
 result = np.where(z > 5, z, 0)
 plt.imshow(result, cmap='gray', extent=[-5, 5, -5, 5])
+
+arr_for_sum = np.random.standard_normal((4, 4))
+arr_for_bool = arr_for_sum > 0
+
+
+print(arr_for_sum.mean())
